4e08936159c155976e94bb8446455c782a830b3d
ankitk26 <myselfankit51@gmail.com>
2025-03-20 02:54:57 +0530

    update middleware matcher

--- middleware.ts
+++ middleware.ts
@@ -11,10 +11,14 @@ export async function middleware(request: NextRequest) {
 
 export const config = {
   matcher: [
-    "/",
-    "/admin",
-    "/transactions",
-    "/insights",
-    "/payees/:receiver_id",
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico, sitemap.xml, robots.txt (metadata files)
+     * - login (authentication page)
+     */
+    "/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|login).*)",
   ],
 };
